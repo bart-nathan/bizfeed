b0d4bcd992a9e275e37cc8b9e0d5a9474aad840a
bart-nathan <johnson_bart@protonmail.ch>
2021-04-12 10:43:02 +0200

    man 12 apr 10:43:02 CEST 2021

--- bizfeed.py
+++ bizfeed.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
+import feedparser
+
+test_link = "https://ekstrabladet.dk/rssfeed/all/"
+
+def get_feeds():
+
+    return feedparser.parse(test_link)["entries"]
+
+
 
 if __name__ == "__main__":
 
-    print("Ok")
+
+    f_list = get_feeds()
+
+    for one_feed in f_list:
+
+        print("%s | %s\n" % (one_feed["published"], one_feed["link"]))
+
+    print("ok")
