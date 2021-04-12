cc10eaddd73c4b1d9ae2ba763c4954e28976614f
bart-nathan <johnson_bart@protonmail.ch>
2021-04-12 10:54:38 +0200

    man 12 apr 10:54:38 CEST 2021

--- bizfeed.py
+++ bizfeed.py
@@ -2,17 +2,39 @@
 
 import feedparser
 
-test_link = "https://ekstrabladet.dk/rssfeed/all/"
+feed_list = []
+feed_list.append(["nyheder", "https://ekstrabladet.dk/rssfeed/all/"] )
+feed_list.append(["nyheder", "https://www.dr.dk/nyheder/service/feeds/allenyheder"] )
 
-def get_feeds():
 
-    return feedparser.parse(test_link)["entries"]
+
+#test_link = "https://ekstrabladet.dk/rssfeed/all/"
+
+def display_feeds():
+
+    for one_feed in feed_list:
+
+        item_list = feedparser.parse(one_feed[1])["entries"]
+
+        for item in item_list:
+
+             print("%s | %s\n" % (item["published"], item["link"]))
+
+            
+        
+
+        print(one_feed)
+    
+
+    #return feedparser.parse(test_link)["entries"]
 
 
 
 if __name__ == "__main__":
 
+    display_feeds()
 
+'''
     f_list = get_feeds()
 
     for one_feed in f_list:
@@ -20,3 +42,5 @@ if __name__ == "__main__":
         print("%s | %s\n" % (one_feed["published"], one_feed["link"]))
 
     print("ok")
+
+'''
